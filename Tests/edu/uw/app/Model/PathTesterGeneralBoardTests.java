f58a5bce2f0c64c5f52e5e649a0dbf75542a4609
RomaBureacov <RomanBureacov@gmail.com>
2025-04-10 01:48:09 -0700

    implemented tests for more broad cases

---
+++ Tests/edu/uw/app/Model/PathTesterGeneralBoardTests.java
@@ -0,0 +1,27 @@
+package edu.uw.app.Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PathTesterGeneralBoardTests {
+
+    private Board iBoard;
+
+    @BeforeEach
+    public void setup() {
+        // to prevent erroneous tests being accidentally written
+        this.iBoard = null;
+    }
+
+    @Test
+    public void testVeryLargeBoard() {
+        final int lVeryLargeSize = 999;
+        this.iBoard = new GameBoard(lVeryLargeSize);
+        PathTestingCommonTests.fillBoard(this.iBoard, 1);
+
+        final int lTarget = lVeryLargeSize * lVeryLargeSize;
+        this.iBoard.set(lVeryLargeSize - 2, lVeryLargeSize - 1, lTarget / 2);
+        this.iBoard.set(lVeryLargeSize - 1, lVeryLargeSize - 1, lTarget / 2);
+        PathTestingCommonTests.testTarget(lTarget, this.iBoard);
+    }
+}
